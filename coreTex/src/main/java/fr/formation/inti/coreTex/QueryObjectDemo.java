6a70de4bf6429c5c50e70c411258e9c51813ad91
0ra3 <51905276+0ra3@users.noreply.github.com>
2019-09-05 16:35:55 +0200

    Hibernate - Exemple 8 passage d'un objet transitoire à un objet Persistant

--- coreTex/src/main/java/fr/formation/inti/coreTex/QueryObjectDemo.java
+++ coreTex/src/main/java/fr/formation/inti/coreTex/QueryObjectDemo.java
@@ -1,5 +1,7 @@
 package fr.formation.inti.coreTex;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
@@ -11,76 +13,67 @@ import org.hibernate.query.Query;
 import fr.formation.inti.beans.ShortEmpInfo;
 import fr.formation.inti.entities.Department;
 import fr.formation.inti.entities.Employee;
+import fr.formation.inti.entities.Timekeeper;
 import fr.formation.inti.service.HibernateUtils;
 import fr.formation.inti.utils.DataUtils;
 
 public class QueryObjectDemo {
 	
-	public static void main(String[] args) {
-		  SessionFactory factory = HibernateUtils.getSessionFactory();
-		  
+	  private static DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+	  
+	   private static Timekeeper persist_Transient(Session session, Employee emp) {
+	  
+            
+	       Timekeeper tk1 = new Timekeeper();
+	 
+	       tk1.setEmployee(emp);
+	       tk1.setInOut(Timekeeper.IN);
+	       tk1.setDateTime(new Date());
+	   
+	       // Now, 'tk1' is transient object
+	       System.out.println("- tk1 Persistent? " + session.contains(tk1));
+	 
+	       System.out.println("====== CALL persist(tk).... ===========");
+	  
+	        
+	       // Hibernate assign value to Id of 'tk1'
+	       // No action to DB.
+	       session.persist(tk1);
+	  
+	       System.out.println("- tk1.getTimekeeperId() = " + tk1.getTimekeeperId());
+	  
+	        
+	       // Now 'tk1' is Persistent object.
+	       // But no action with DB.
+	       // ==> true
+	       System.out.println("- tk1 Persistent? " + session.contains(tk1));
+	 
+	       System.out.println("- Call flush..");
+	  
+	        
+	       // Flush data to DB.
+	       // Hibernate execute insert statement.
+	       session.flush();
+	 
+	       String timekeeperId = tk1.getTimekeeperId();
+	       System.out.println("- timekeeperId = " + timekeeperId);
+	       System.out.println("- inOut = " + tk1.getInOut());
+	       System.out.println("- dateTime = " + df.format(tk1.getDateTime()));
+	       System.out.println();
+	       return tk1;
+	   }
+	 
+	   public static void main(String[] args) {
+	       SessionFactory factory = HibernateUtils.getSessionFactory();
+	 
 	       Session session = factory.getCurrentSession();
-	       Department department = null;
-	 
+	       Employee emp = null;
 	       try {
 	           session.getTransaction().begin();
 	 
-	           System.out.println("- Finding Department deptNo = D10...");
-	       
-	           // Récupératiion d'un objet persistant.
-	           department = DataUtils.findDepartment(session, "D10");
+	           emp = DataUtils.findEmployee(session, "E7499");
 	 
-	           System.out.println("- First change Location");
-	      
-	            
-	           // Modifier un objet persistant.
-	           department.setLocation("Chicago " + System.currentTimeMillis());
-	            
-	           System.out.println("- Location = " + department.getLocation());
-	 
-	           System.out.println("- Calling flush...");
-	    
-	           // session.flush() permet d'insérer les changement dans la DB.
-	           session.flush();
-	 
-	           System.out.println("- Flush OK");
-	 
-	           System.out.println("- Second change Location");
-	            
-	          
-	        // -----------------------------------------------------------------\\
-	           
-	           
-	           department.setLocation("Chicago " + System.currentTimeMillis());
-	  
-	           // Print out location
-	           System.out.println("- Location = " + department.getLocation());
-	 
-	           System.out.println("- Calling commit...");
-	  
-	           // Commit
-	           session.getTransaction().commit();
-	 
-	           System.out.println("- Commit OK");
-	           
-	       } catch (Exception e) {
-	           e.printStackTrace();
-	           session.getTransaction().rollback();
-	       }
-	  
-	       // Create the session after it had been closed earlier
-	       // (Cause by commit or update)
-	       session = factory.getCurrentSession();
-	       try {
-	           session.getTransaction().begin();
-	 
-	           System.out.println("- Finding Department deptNo = D10...");
-	            
-	           // Recherche du département
-	           department = DataUtils.findDepartment(session, "D10");
-	  
-
-	           System.out.println("- D10 Location = " + department.getLocation());
+	           persist_Transient(session, emp);
 	 
 	           session.getTransaction().commit();
 	       } catch (Exception e) {
